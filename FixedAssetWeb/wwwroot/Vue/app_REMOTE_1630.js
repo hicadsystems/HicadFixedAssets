4861c99339afe3978fb66b72fe0198ea93995933
Your Name <iamKosmicblast@gmail.com>
2021-08-23 15:58:32 +0100

    Adding Delete Functionality

---
+++ FixedAssetWeb/wwwroot/Vue/app_REMOTE_1630.js
@@ -0,0 +1,37 @@
+window.Vue = require('vue')
+window.axios = require('axios')
+import store from './store';
+import VueSimpleAlert from "vue-simple-alert";
+
+Vue.use(VueSimpleAlert);
+
+import CreateCompanyComponent from './components/CreateCompanyComponent.vue';
+import ViewCompanyComponent from './components/ViewCompanyComponent.vue';
+import CreateCostCenter from './components/CreateCostCenter.vue';
+import ViewCostCenter from './components/ViewCostCenter.vue';
+import CreateBusinesslineComponent from './components/CreateBusinesslineComponent.vue';
+import ViewBusinesslineComponent from './components/ViewBusinesslineComponent.vue';
+import AssetReclassificationComponent from './components/Transactions/AssetReclassificationComponent.vue';
+import AssetMovementComponent from './components/Transactions/AssetMovementComponent.vue';
+import AssetRegistrationComponent from './components/AssetRegistrationComponent.vue';
+import AssetClass from './components/AssetClass.vue';
+import Vue from 'vue';
+
+const files = require.context('./', true, /\.vue$/i)
+files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
+
+Vue.component('create-company-component', CreateCompanyComponent);
+Vue.component('view-company-component',ViewCompanyComponent);
+Vue.component('create-cost-center',CreateCostCenter);
+Vue.component('view-cost-center',ViewCostCenter);
+Vue.component('create-businessline-component',CreateBusinesslineComponent);
+Vue.component('view-businessline-component',ViewBusinesslineComponent);
+Vue.component('asset-reclassification-component', AssetReclassificationComponent);
+Vue.component('asset-movement-component', AssetMovementComponent);
+Vue.component('asset-registration-component', AssetRegistrationComponent);
+Vue.component('asset-class', AssetClass);
+ 
+const app = new Vue({
+    el: "#app",
+    store
+});
