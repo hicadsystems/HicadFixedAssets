93e4536df7dcf42201f87764b52bca252f7310d0
Your Name <iamKosmicblast@gmail.com>
2021-09-24 11:36:49 +0100

    Adding Asset Disposal Table

---
+++ FixedAssetCore/Entities/fa_assetDisposal.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace FixedAssetCore.Entities
+{
+    public class fa_assetDisposal
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(20)]
+        public string assetCode { get; set; }
+
+        [StringLength(100)]
+        public string assetDesc { get; set; }
+
+        [StringLength(2)]
+        public string Class { get; set; }
+
+        [StringLength(15)]
+        public string Dept { get; set; }
+
+        [StringLength(2)]
+        public string Busline { get; set; }
+        public DateTime? Purchdate { get; set; }
+
+        public DateTime? Revaldate { get; set; }
+
+        public DateTime? Reclassdate { get; set; }
+        public DateTime? movedate { get; set; }
+
+        public DateTime? Dispdate { get; set; }
+
+        public decimal? Purchval { get; set; }
+        public decimal? Accum_depre { get; set; }
+        public decimal? Dispval { get; set; }
+
+        public decimal? Revalval { get; set; }
+        public DateTime? Insurdate { get; set; }
+        public decimal? Insuredval { get; set; }
+        public decimal? year_depr { get; set; }
+        public decimal? depr_rate { get; set; }
+    }
+}
---
+++ FixedAssetWeb/Migrations/20210924103424_Adding fa_assetDisposal table.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FixedAssetWeb.Migrations
+{
+    public partial class Addingfa_assetDisposaltable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "fa_AssetDisposals",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    assetCode = table.Column<string>(maxLength: 20, nullable: true),
+                    assetDesc = table.Column<string>(maxLength: 100, nullable: true),
+                    Class = table.Column<string>(maxLength: 2, nullable: true),
+                    Dept = table.Column<string>(maxLength: 15, nullable: true),
+                    Busline = table.Column<string>(maxLength: 2, nullable: true),
+                    Purchdate = table.Column<DateTime>(nullable: true),
+                    Revaldate = table.Column<DateTime>(nullable: true),
+                    Reclassdate = table.Column<DateTime>(nullable: true),
+                    movedate = table.Column<DateTime>(nullable: true),
+                    Dispdate = table.Column<DateTime>(nullable: true),
+                    Purchval = table.Column<decimal>(nullable: true),
+                    Accum_depre = table.Column<decimal>(nullable: true),
+                    Dispval = table.Column<decimal>(nullable: true),
+                    Revalval = table.Column<decimal>(nullable: true),
+                    Insurdate = table.Column<DateTime>(nullable: true),
+                    Insuredval = table.Column<decimal>(nullable: true),
+                    year_depr = table.Column<decimal>(nullable: true),
+                    depr_rate = table.Column<decimal>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_fa_AssetDisposals", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "fa_AssetDisposals");
+        }
+    }
+}
